36fb062ffa74ee0fa9a4d8ff900575e48d8c37d0
aniwag2 <aniwaghray@gmail.com>
2025-04-23 23:52:43 -0400

    Fixed some issues that deployment was giving

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,47 @@
 import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 
-
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css'
 
-const INITIAL_CENTER = [
-  -86.2520,
-  41.6764
-]
+const INITIAL_CENTER: [number, number] = [-86.2520, 41.6764]
 const INITIAL_ZOOM = 12.86
 
 function App() {
-  const mapRef = useRef()
-  const mapContainerRef = useRef()
+  const mapRef = useRef<mapboxgl.Map | null>(null)
+  const mapContainerRef = useRef<HTMLDivElement | null>(null)
 
-  const [center, setCenter] = useState(INITIAL_CENTER)
+  const [center, setCenter] = useState<[number, number]>(INITIAL_CENTER)
   const [zoom, setZoom] = useState(INITIAL_ZOOM)
 
   useEffect(() => {
-    console.log(import.meta.env.VITE_MAPBOX_TOKEN)
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
+
+    if (!mapContainerRef.current) return
+
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       center: center,
-      zoom: zoom
+      zoom: zoom,
+      style: 'mapbox://styles/mapbox/streets-v11',
     });
 
     mapRef.current.on('move', () => {
-      // get the current center coordinates and zoom level from the map
-      const mapCenter = mapRef.current.getCenter()
-      const mapZoom = mapRef.current.getZoom()
-
-      // update state
-      setCenter([ mapCenter.lng, mapCenter.lat ])
+      const mapCenter = mapRef.current!.getCenter()
+      const mapZoom = mapRef.current!.getZoom()
+      setCenter([mapCenter.lng, mapCenter.lat])
       setZoom(mapZoom)
     })
 
     return () => {
-      mapRef.current.remove()
+      mapRef.current?.remove()
     }
   }, [])
 
   const handleButtonClick = () => {
-    mapRef.current.flyTo({
+    mapRef.current?.flyTo({
       center: INITIAL_CENTER,
-      zoom: INITIAL_ZOOM
+      zoom: INITIAL_ZOOM,
     })
   }
 
